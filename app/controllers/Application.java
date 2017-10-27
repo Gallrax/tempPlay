d7ac40840b0cba7bad4fbbd12f4dca6461ddaf98
Gallrax <827269727@qq.com>
2017-10-27 17:42:07 +0800

    增加show

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,4 +20,9 @@ public class Application extends Controller {
         render(frontPost, olderPosts);
     }
 
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+
 }
